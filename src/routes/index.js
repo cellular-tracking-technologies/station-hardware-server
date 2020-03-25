65d25ac38adc301b6e2e92ecf05c1859c08bf7f1
Sheldon Blackshire <sheldon.blackshire@celltracktech.com>
2020-03-25 20:54:00 +0000

    added /node/version endpoint that gets the package name and version of all node projects in the /home/pi/ctt path

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,60 @@
 var express = require('express');
 var router = express.Router();
 const { exec } = require('child_process');
+var fs = require('fs');
+var path = require('path');
+
+class FileFinder {
+    constructor(options) {
+        this.dir = options.dir;
+        this.ignore = options.ignore;
+        this.match = options.match;
+    }
+    scan() {
+        return this.walkSync_(this.dir);
+    }
+    walkSync_(dir) {
+        let list = [];
+        let files = fs.readdirSync(dir);
+        files.forEach((file) => {
+            if (fs.statSync(path.join(dir, file)).isDirectory()) {
+                if (file.match(this.ignore) == null) {
+                    list.push(...this.walkSync_(path.join(dir, file)));
+                }
+            }
+            else {
+                if (file.match(this.match) != null) {
+                    list.push(path.join(dir, file));
+                }
+            }
+        });
+        return list;
+    }
+}
+
+function GetPackageVersions() {
+
+    let finder = new FileFinder({
+        dir: '/home/pi/ctt',
+        ignore: 'node_modules',
+        match: 'package.json'
+    });
+
+    let list = [];
+    finder.scan().forEach(file => {
+        try {
+            const contents = JSON.parse(fs.readFileSync(file, 'utf8'));
+
+            list.push({
+                name:contents.name,
+                version:contents.version
+            })
+        } catch (err) {
+            throw err;
+        }
+    });
+    return list;
+}
 
 function deviceId(){
     return new Promise((resolve, reject) =>{ 
@@ -33,6 +87,10 @@ router.get('/', function(req, res, next) {
 
 router.get('/id', function(req, res, next) {
     res.json({id: device_id});
+});
+
+router.get('/node/version', function(req, res, next) {
+    res.json(GetPackageVersions());
 });
 
 module.exports = router;
