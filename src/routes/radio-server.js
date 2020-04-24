f38e2156822b64baf38d4bf13dc151a46b431543
Bob Fogg <philomachus@gmail.com>
2020-04-24 18:18:17 +0100

    get station config document

--- src/routes/radio-server.js
+++ src/routes/radio-server.js
@@ -1,30 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const fs = require('fs');
+const express = require('express');
+const router = express.Router();
 const WebSocket = require('ws');
-const WebSocketURL = 'http://localhost:8001';
+const WebSocketURL = 'ws://localhost:8001';
+const ConfigFileURI = '/etc/ctt/station-config.json';
 
-router.get('/', (req,res,next) => {
-  res.json({wtf:true});
-});
-
-router.get('/stats', (req, res, next) => {
-  let ws = new WebSocket(WebSocketURL);
-  ws.on('open', () => {
-    ws.send(JSON.stringify({
-      msg_type: 'cmd',
-      cmd: 'stats'
-    }));
-    ws.on('message', (msg) => {
-      msg = JSON.parse(msg);
-      if (msg.msg_type == 'stats') {
-        ws.close();
-        res.json(msg);
-      }
-    })
-    ws.on('close', () => {
-      console.log('ws closed')
-    })
-  });
+router.get('/config', (req, res, next) => {
+  try {
+    let config = JSON.parse(fs.readFileSync(ConfigFileURI).toString());
+    res.json(config);
+  } catch (err) {
+    res.json({err: err.toString()});
+  }
 });
 
 module.exports = router;
