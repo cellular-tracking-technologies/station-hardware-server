e8092593a69a0c6bab8ce09fd005a340d0a1c747
Bob Fogg <philomachus@gmail.com>
2020-04-30 17:13:36 +0100

    parse lsub return json object

--- src/routes/peripherals.js
+++ src/routes/peripherals.js
@@ -3,12 +3,31 @@ const router = express.Router()
 const { exec }= require('child_process');
 
 router.get('/', function(req, res, next) {
+  // run lsusb and parse the results
   exec("lsusb", (err, stdout, stderr) => {
     if (err) {
       res.json({'error': err.toString()});
     }
+    let fields;
+    let lines = stdout.split('\n');
+    let records = [];
+    let ids, values;
+    // for each lsusb record - pull out device id, vendor id, product id and name
+    lines.forEach((line) => {
+      fields = line.split(' ');
+      if (fields.length > 6) {
+        ids = fields[5];
+        values = ids.split(':');
+        records.push({
+          device: fields[3],
+          vendor: values[0],
+          product: values[1],
+          name: fields.slice(6,).join(' ').trim()
+        });
+      }
+    });
     res.json({
-      info: stdout
+      info: records
     });
   });
 });
