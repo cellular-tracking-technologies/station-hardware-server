fd513012154b9b083405abefd8ee12b31022738a
Bob Fogg <philomachus@gmail.com>
2020-05-12 17:32:05 +0100

    add checkin to server via web socket

--- src/routes/radio-server.js
+++ src/routes/radio-server.js
@@ -5,6 +5,25 @@ const WebSocket = require('ws');
 const WebSocketURL = 'ws://localhost:8001';
 const ConfigFileURI = '/etc/ctt/station-config.json';
 
+router.get('/checkin', (req, res, next) => {
+  let ws = new WebSocket(WebSocketURL);
+  ws.on('open', () => {
+    ws.send(JSON.stringify({
+      msg_type: 'cmd',
+      cmd: 'checkin'
+    }));
+    res.send(204);
+  });
+  ws.on('close', () => {
+    res.status(500).send('web socket closed');
+  });
+  ws.on('error', (err) => {
+    console.error('error connected to radio server web socket');
+    console.error(err);
+    res.status(500).send('web socket connect error');
+  });
+});
+
 router.get('/config', (req, res, next) => {
   try {
     let config = JSON.parse(fs.readFileSync(ConfigFileURI).toString());
