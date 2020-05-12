b693279eabadb281ac39ac1aa2a8272c6372cfd5
Bob Fogg <philomachus@gmail.com>
2020-05-12 17:32:23 +0100

    add modem controls

--- src/routes/modem.js
+++ src/routes/modem.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const { spawn } = require('child_process');
 
 const { ModemInterface, QuectelCommandSetParser } = require('@cellular-tracking-technologies/modem-status-driver');
 
@@ -14,6 +15,67 @@ Modem.open();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json(Modem.info);
+});
+
+const RunCommand = (cmd, args) => {
+  console.log('about to run command', cmd);
+  return new Promise((resolve, reject) => {
+    const command_process = spawn(cmd, args);
+    command_process.stdout.on('data', (data) => {
+      console.log(data.toString());
+    });
+    command_process.stderr.on('data', (data) => {
+      console.error(data.toString());
+    });
+    command_process.on('close', (code) => {
+      resolve(code);
+    });
+    command_process.on('error', (err) => {
+      console.error('error processing command', cmd);
+      console.error(err);
+      reject(err);
+    });
+  });
+};
+
+router.post('/stop', (req, res, next) => {
+  RunCommand('systemctl', ['stop', 'modem'])
+    .then((response) => {
+      res.status(204).send();
+    })
+    .catch((err) => {
+      res.status(500).send(err.toString());
+    })
+});
+
+router.post('/start', (req, res, next) => {
+  RunCommand('systemctl', ['start', 'modem'])
+    .then((response) => {
+      res.status(204).send();
+    })
+    .catch((err) => {
+      res.status(500).send(err.toString());
+    })
+});
+
+router.post('/enable', (req, res, next) => {
+  RunCommand('systemctl', ['enable', 'modem'])
+    .then((response) => {
+      res.status(204).send();
+    })
+    .catch((err) => {
+      res.status(500).send(err.toString());
+    })
+});
+
+router.post('/disable', (req, res, next) => {
+  RunCommand('systemctl', ['disable', 'modem'])
+    .then((response) => {
+      res.status(204).send();
+    })
+    .catch((err) => {
+      res.status(500).send(err.toString());
+    })
 });
 
 module.exports = router;
